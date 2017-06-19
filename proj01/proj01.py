c63d8a1f2df45ae05c0334b4c7bec83729b9c3e7
Bonnie-Lin <bonnietheawesome@icloud.com>
2017-06-19 11:29:01 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -7,4 +7,20 @@
 
 # If you complete extensions, describe your extensions here!
 
+print "hello world!" \
+
+print "Hello, user! Will you please tell me your name?"
+N = raw_input ("Enter Name: ")
+print N, "...Interesting name! How old are you?"
+A = raw_input ("Age: ")
+print "what year is it?"
+Y = raw_input ("")
+F = 100 - int(A)
+G = 2017 + F
+H = G - 1
+print "have you had your birthday this year?"
+B = raw_input ("Enter Yes or No")
+#if B == No:
+    print "The year you will turn 100 years old will be... int(H)"
+#else print "The year you will turn 100 years old will be... int(G)"
 
