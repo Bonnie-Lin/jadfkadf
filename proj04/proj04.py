9a71bd953879a3106f34866e36b71d1b19a6bbfa
Bonnie-Lin <bonnietheawesome@icloud.com>
2017-06-20 15:27:35 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -7,4 +7,22 @@ proj04
 Asks the user for a string and prints out whether or not the string is a palindrome.
 
 """
+userinputlist2 = []
+userinput = raw_input ("Enter a string!")
+userinputlist = []
+for letters in userinput:
+    userinputlist.append(letters)
+
+    length = len(userinputlist)
+    userinputlist2 = [letters] + userinputlist2
+if userinputlist == userinputlist2:
+    print "Your string is a palindrome"
+else:
+    print "Your string is not a palindrome"
+print "your word backwards is:"
+print userinputlist2
+
+
+
+
 
